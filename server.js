84cc1db93617cebb41151416e55e08b51a724d57
AleksandrHnut <aleksandr.hnut@gmail.com>
2022-09-23 18:23:42 +0300

    Обновляет структуру проекта

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const params = {
 	host: 'localhost', // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
 	ignore: 'node_modules', // comma-separated string for paths to ignore
 	logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+	root: './public',
 };
 
 liveServer.start(params);
